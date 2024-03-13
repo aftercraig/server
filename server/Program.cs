346acc90644060ebc7e456746960f27e8700a5b8
IS22-11 <IS22-11@MK-104-025>
2024-03-13 11:16:58 +0500

    Добавьте файлы проекта.

---
+++ server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+
+    System.Console.WriteLine("Введите команду для сервера");
+    await tcpClient.ConnectAsync("192.168.220.241", 12345);
+    while (true) 
+    {
+
+        string command = Console.ReadLine() + '\n';
+        byte[] requestData = Encoding.UTF8.GetBytes(command);
+        await tcpClient.SendAsync(requestData, new SocketFlags());
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
